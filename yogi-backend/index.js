47131e40103b28d825da50b8b8c6b415331ed5c4
Thomas McLaughlin <123tommygun1@gmail.com>
2023-04-28 12:08:28 -0400

    config render, mongo, expo deployed

--- yogi-backend/index.js
+++ yogi-backend/index.js
@@ -1,93 +1,73 @@
-const express = require('express');
-// to avoid cross-origin resource sharing issue when debugging on web
-const cors = require('cors');
-// Netflix dependencies
-const Netflix = require('netflix2');
+const express = require("express");
+const cors = require("cors");
+const Netflix = require("netflix2");
 const netflix = new Netflix();
-// JSON dependencies for login stuff
-const bodyParser = require('body-parser');
-const jsonfile = require('jsonfile');
-// import bcrypt for hashing passwords
-const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const { MongoClient } = require("mongodb");
 
 const app = express();
-
-//enables web debug (hopefully)
-app.use(cors());
 const port = 3000;
-const usersFile = '/home/halincandenza/personalProjects/realReactTestrun/users.json';
 
-// Middleware for parsing JSON request bodies
+app.use(cors());
 app.use(bodyParser.json());
 
-// Signup route
-app.post('/signup', cors(), async (req, res) => {
-    console.log(req, "this is req")
-  const { email, password } = req.body;
+// Replace with your MongoDB Atlas connection string
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+let usersCollection;
 
-  
-  // bcrypt hashing
+MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to MongoDB Atlas");
+    const db = client.db("myDatabase");
+    usersCollection = db.collection("users");
+
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB Atlas", error);
+  });
+
+app.post("/signup", cors(), async (req, res) => {
+  const { email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  
-  jsonfile.readFile(usersFile, (err, users) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
+  const existingUser = await usersCollection.findOne({ email });
 
-    if (users.some((user) => user.email === email)) {
-      res.status(400).json({ error: 'User with this email already exists' });
-      return;
-    }
+  if (existingUser) {
+    res.status(400).json({ error: "User with this email already exists" });
+    return;
+  }
 
-    const newUser = { email, hashedPassword }; //storing hashed pw
-    users.push(newUser);
-
-    jsonfile.writeFile(usersFile, users, (writeErr) => {
-      if (writeErr) {
-        res.status(500).json({ error: writeErr.message });
-      } else {
-        res.status(201).json(newUser);
-      }
-    });
-  });
+  const newUser = { email, hashedPassword };
+  await usersCollection.insertOne(newUser);
+  res.status(201).json(newUser);
 });
 
-// Login route
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-  
-    jsonfile.readFile(usersFile, async (err, users) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-  
-      const user = users.find((u) => u.email === email);
-  
-      if (user) {
-        // comparing submitted password with the stored hashed password
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersCollection.findOne({ email });
 
-        const passwordMatch = await bcrypt.compare(password, user.hashedPassword);
-  
-        if (passwordMatch) {
-          res.status(200).json({ email: user.email });
-          console.log("password match")
-        } else {
-          res.status(401).json({ error: 'Invalid email or password' });
-        }
-      } else {
-        res.status(401).json({ error: 'Invalid email or password' });
-      }
-    });
-  });
+  if (user) {
+    const passwordMatch = await bcrypt.compare(password, user.hashedPassword);
 
-// Netflix viewing history route
-app.get('/viewing-history', async (req, res) => {
+    if (passwordMatch) {
+      res.status(200).json({ email: user.email });
+      console.log("password match");
+    } else {
+      res.status(401).json({ error: "Invalid email or password" });
+    }
+  } else {
+    res.status(401).json({ error: "Invalid email or password" });
+  }
+});
+
+app.get("/viewing-history", async (req, res) => {
   const credentials = {
-    email: 'mclaugth01@esj.org',
-    password: '#######',
+    email: "#########",
+    password: "#######",
   };
 
   try {
@@ -111,9 +91,4 @@ app.get('/viewing-history', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-});
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
 });
